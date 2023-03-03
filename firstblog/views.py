e6bcd84a5f752f41382e07729adc17a7b22caba0
Mathew Legge <sulelegge@gmail.com>
2023-03-03 17:58:16 +0300

    New update

--- firstblog/views.py
+++ firstblog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from .form import UserRegistrationForm
 
 def home(request):
-    form = UserCreationForm()
+    form = UserRegistrationForm()
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             try:
                 form.save()
