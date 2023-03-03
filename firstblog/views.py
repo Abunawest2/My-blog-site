76fca3ac213e70eb86ca3f1a175ed3b0bd619d6e
Mathew Legge <sulelegge@gmail.com>
2023-03-03 16:23:20 +0300

    Just some bit of views update

--- firstblog/views.py
+++ firstblog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def home(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'main/index.html', context)
