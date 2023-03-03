58d0b3dac01df410635bfac1479be2fc08e1261b
Mathew Legge <sulelegge@gmail.com>
2023-03-03 18:55:35 +0300

    update

--- firstblog/form.py
+++ firstblog/form.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class UserRegistrationForm(UserCreationForm):
-
+class UserForm(UserCreationForm):
     class Meta:
-        models = User
+        model = User
         fields = ['username', 'email', 'password1', 'password2']
