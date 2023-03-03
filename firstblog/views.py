18cd8d07ddaaf0650c10ae2dc2b378ae749e3aba
Mathew Legge <sulelegge@gmail.com>
2023-03-03 17:23:05 +0300

    Updating views

--- firstblog/views.py
+++ firstblog/views.py
@@ -3,9 +3,14 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
+    form = UserCreationForm()
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            try:
+                form.save()
+            except Exception as e:
+                print(e)
     context = {'form':form}
     return render(request, 'main/index.html', context)
+
