4440284910a898be20e4adc08b007e48ab3dd2b7
Mathew Legge <sulelegge@gmail.com>
2023-03-05 15:07:15 +0300

    A bit of editing

--- firstblog/models.py
+++ firstblog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
-# Create your models here.
+class BlogPost(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    post = models.TextField()
+    Date_Created = models.DateField(auto_now_add=True)
+    Date_Created = models.DateField(auto_now=True)
+
+# class CustomerUser(AbstractUser):
+#     pass
