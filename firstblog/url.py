c3611ea565a4fdc24c30f053c362c69c24c69b82
Mathew Legge <sulelegge@gmail.com>
2023-03-05 15:07:15 +0300

    A bit of editing

--- firstblog/url.py
+++ firstblog/url.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views as v
 
 urlpatterns = [
-    path('', v.home),
+    path('', v.home, name='home'),
 ]
--- firstblog/views.py
+++ firstblog/views.py
@@ -2,16 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from .form import UserForm
+from .models import BlogPost
+
+# def home(request):
+#     form = UserForm()
+#     if request.method == "POST":
+#         form = UserForm(request.POST)
+#         if form.is_valid():
+#             try:
+#                 form.save()
+#             except Exception as e:
+#                 print(e)
+#     context = {'form':form}
+
+#     return render(request, 'main/index.html', context)
+
 
 def home(request):
-    form = UserForm()
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        if form.is_valid():
-            try:
-                form.save()
-            except Exception as e:
-                print(e)
-    context = {'form':form}
+    posts = BlogPost.objects.all()
+    context = {'posts': posts}
     return render(request, 'main/index.html', context)
 
