c367563d192634df7750dfd178b2d8ed21a431b7
Mathew Legge <sulelegge@gmail.com>
2023-03-09 02:00:21 +0300

    New work been put in such as rendering post with images

--- firstblog/models.py
+++ firstblog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, User
+from django.contrib.auth.models import AbstractUser, User, AbstractBaseUser
+from multiselectfield import MultiSelectField
 
 class BlogPost(models.Model):
-    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     add_image = models.ImageField(null=True, blank=True, upload_to='image/')
     post = models.TextField()
     date_created = models.DateField(auto_now_add=True)
     date_updated = models.DateField(auto_now=True)
 
-
-# class CustomerUser(AbstractUser):
+# class CustomerUser(AbstractBaseUser):
 #     pass
