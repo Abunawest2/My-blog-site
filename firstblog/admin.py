0c9ad74960e83830ba26d89a63f31d2d1f9c13b4
Mathew Legge <sulelegge@gmail.com>
2023-03-12 22:15:46 +0300

    LATEST commit

--- firstblog/admin.py
+++ firstblog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
+CustomUser = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(BlogPost)
 admin.site.register(Topic)
+
--- firstblog/form.py
+++ firstblog/form.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import BlogPost, CustomUser
+from django.core.validators import FileExtensionValidator
+
+
 class UserForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
@@ -23,5 +27,28 @@ class UserForm(UserCreationForm):
         )
     )
     class Meta:
-        model = User
+        model = CustomUser
         fields =  ['username', 'email', 'password1', 'password2']
+
+class BlogPostForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs = {'class':'form-in form-control'}
+        ),
+        required=True
+    )
+    add_image = forms.ImageField(
+        widget=forms.FileInput(
+            attrs = {'class':'form-in form-control'}
+        ),
+        required=False,
+        validators=[FileExtensionValidator(['jpg', 'jpeg', 'png', 'gif'])]
+    )
+    post = forms.CharField(
+        widget=forms.Textarea(
+            attrs = {'class':'form-in form-control'}
+        )
+    )
+    class Meta:
+        model = BlogPost
+        fields = ['topic', 'title', 'add_image', 'post']
